0184a936496ca6fdd0d9a40784bbdbfdd84be3fd
Ben Wang <ben.wang@c3.co.nz>
2020-02-13 17:06:57 +1300

    test navigation demo

---
+++ src/scripts/test_navigation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+xterm  -e  " source catkin_ws/devel/setup.bash; roscore" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+
+
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e   " roslaunch turtlebot_teleop keyboard_teleop.launch"
