97bc42ea49d6a23b6645239d0c685f71f34c9fc0
Ben Wang <ben.wang@c3.co.nz>
2020-02-13 17:09:22 +1300

    create pick_objects package

---
+++ src/scripts/pick_objects.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+xterm  -e  " source catkin_ws/devel/setup.bash; roscore" &
+sleep 3
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm  -e  " rosrun pick_objects pick_objects"
+sleep 3
