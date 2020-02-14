7528d0f50ddcd721777d79701dc7be4c66b20d40
Ben Wang <ben.wang@c3.co.nz>
2020-02-14 13:42:47 +1300

    display and hide the virtual object which is manually added in the rviz

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -4,83 +4,86 @@
 
 //void callbackFunc(const nav_msg);
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "add_markers");
-    ros::NodeHandle n;
-    ros::Rate r(1);
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-
+void displayVirtualObject(ros::Publisher marker_pub,
+                          const char* info,
+                        float color_a = 1.0,
+                        float pos_x = 0.0, float pos_y = 0.0,
+                        float o_x = 0.0, float o_y = 0.0, float o_z = 0.0, float o_w = 1.0
+                        ) {
 
     // Set our initial shape type to be a cube
     uint32_t shape = visualization_msgs::Marker::CUBE;
 
-    while (ros::ok()) {
-        visualization_msgs::Marker marker;
-        // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-        marker.header.frame_id = "/map";  // Fixed Frame in the rviz.
-                                        // Without this, rviz can't display the marker,
-                                        // although topic visualization_marker still has the info
-        marker.header.stamp = ros::Time::now();
+    // display object in pick up zone for 5 second
+    visualization_msgs::Marker marker;
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "/map";  // Fixed Frame in the rviz.
+    // Without this, rviz can't display the marker,
+    // although topic visualization_marker still has the info
+    marker.header.stamp = ros::Time::now();
 
-        // Set the namespace and id for this marker.  This serves to create a unique ID
-        // Any marker sent with the same namespace and id will overwrite the old one
-        marker.ns = "add_markers";
-        marker.id = 0;
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = "add_markers";
+    marker.id = 0;
 
-        // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-        marker.type = shape;
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    marker.type = shape;
 
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        marker.action = visualization_msgs::Marker::ADD;
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = visualization_msgs::Marker::ADD;
 
-        // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-        marker.pose.position.x = 0;
-        marker.pose.position.y = 0;
-        marker.pose.position.z = 0;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = pos_x;
+    marker.pose.position.y = pos_y;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = o_x;
+    marker.pose.orientation.y = o_y;
+    marker.pose.orientation.z = o_z;
+    marker.pose.orientation.w = o_w;
 
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = 0.5;
+    marker.scale.y = 0.5;
+    marker.scale.z = 0.5;
 
-        // Set the color -- be sure to set alpha to something non-zero!
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        marker.color.a = 1.0;
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = color_a;
 
-        marker.lifetime = ros::Duration();
+    marker.lifetime = ros::Duration();
 
-        // Publish the marker
-        while (marker_pub.getNumSubscribers() < 1) {
-            if (!ros::ok()) {
-                return 0;
-            }
-            ROS_WARN_ONCE("Please create a subscriber to the marker");
-            sleep(1);
+    // Publish the marker
+    while (marker_pub.getNumSubscribers() < 1) {
+        if (!ros::ok()) {
+            return;
         }
-        marker_pub.publish(marker);
-
-        // Cycle between different shapes
-        switch (shape) {
-            case visualization_msgs::Marker::CUBE:
-                shape = visualization_msgs::Marker::SPHERE;
-                break;
-            case visualization_msgs::Marker::SPHERE:
-                shape = visualization_msgs::Marker::ARROW;
-                break;
-            case visualization_msgs::Marker::ARROW:
-                shape = visualization_msgs::Marker::CYLINDER;
-                break;
-            case visualization_msgs::Marker::CYLINDER:
-                shape = visualization_msgs::Marker::CUBE;
-                break;
-        }
-
-        r.sleep();
+        ROS_WARN_ONCE("Please create a subscriber to the marker");
+        sleep(1);
     }
+    ROS_INFO(info);
+    marker_pub.publish(marker);
+
+    return;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "add_markers");
+    ros::NodeHandle n;
+//    ros::Rate r(0.2); // 1/5 => 5sec
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+    while(true) {
+        displayVirtualObject(marker_pub, "display", 1.0);
+        sleep(5);
+
+        // hide the object for 5 seconds
+        displayVirtualObject(marker_pub, "hide", 0.0);
+        sleep(5);
+    }
+
+    return 0;
+
 }
