2be761c117b5ee508fa495d368557030d3227f07
Ben Wang <ben.wang@c3.co.nz>
2020-02-20 11:19:09 +1300

    add map to repo

--- src/scripts/test_my_robot.sh
+++ src/scripts/test_my_robot.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
-
-xterm  -e  " source /home/kerry/catkin_ws/devel/setup.bash; roscore" &
-sleep 5
-xterm  -e  " roslaunch my_robot world.launch" &
-sleep 5
-xterm -e " roslaunch my_robot amcl.launch" &
-sleep 5
-# either use keyboard or 2d nav goal from Rviz
-xterm -e   " rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
+##!/bin/sh
+#
+#xterm  -e  " source /home/kerry/catkin_ws/devel/setup.bash; roscore" &
+#sleep 5
+#xterm  -e  " roslaunch my_robot world.launch" &
+#sleep 5
+#xterm -e " roslaunch my_robot amcl.launch" &
+#sleep 5
+## either use keyboard or 2d nav goal from Rviz
+#xterm -e   " rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -5,7 +5,12 @@ sleep 5
 
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+
+# gmapping_demo also works
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+#
+#xterm  -e  " roslaunch gmapping slam_gmapping_pr2.launch" &
+
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
